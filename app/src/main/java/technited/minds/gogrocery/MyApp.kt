f6e3ef6149a2b8b7971fe34da39692e5d4f6b9a6
Adarsh <sahuadarh0@gmail.com>
2021-06-02 21:41:59 +0530

    added CircleCI

--- app/src/main/java/technited/minds/gogrocery/MyApp.kt
+++ app/src/main/java/technited/minds/gogrocery/MyApp.kt
@@ -1,6 +1,18 @@
 package technited.minds.gogrocery
 
 import android.app.Application
+import androidx.databinding.library.baseAdapters.BuildConfig
+import com.rezwan.knetworklib.KNetwork
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
+@HiltAndroidApp
 class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KNetwork.initialize(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }

638c6771f9b5dc27f7f8ff4a78fa8c681ac5101d
Adarsh <sahuadarh0@gmail.com>
2021-06-02 13:06:24 +0530

    Migrating to MVVM Jetpack Kotlin

---
+++ app/src/main/java/technited/minds/gogrocery/HomeActivity.kt
@@ -0,0 +1,11 @@
+package technited.minds.gogrocery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
